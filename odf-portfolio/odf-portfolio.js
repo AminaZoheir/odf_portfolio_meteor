82beefd33639a5b3b745430caf24efc9633911c1
Mohamed Salah <msalah93@Mohameds-MacBook-Pro-5.local>
2015-12-06 16:42:25 +0200

    Code Skeleton

--- odf-portfolio/odf-portfolio.js
+++ odf-portfolio/odf-portfolio.js
@@ -1,17 +1,52 @@
 Projects = new Mongo.Collection("projects");
+// projects.
+
+Router.route('/',{
+  template: 'home'
+});
+Router.configure({
+  layoutTemplate: 'main-layout'
+});
+Router.route('/portfolio/:_id', {
+    template: 'projectPage',
+    data: function(){
+      var currProj = this.params._id;
+      console.log(currProj);
+      return Projects.findOne({_id: currProj});
+    }
+});
+Router.route('/portfolio');
 
 if (Meteor.isClient) {
 
   // This code only runs on the client
 
-  Template.body.helpers({
-
+  Template.portfolio.helpers({
     projects: function(){
+      if(Session.get("currCat")){
+        if(Session.get("currSubCat"))
+          return Projects.find({category:Session.get("currCat"), subcategory:Session.get("currSubCat")});
+        return Projects.find({category:Session.get("currCat")});
+      }
       return Projects.find({});
-    }
+    },
+    categories: [
+                {name:"Architecture",sub:["Complexes","Hotels","Residential Buildings","Residential Compounds","Admin","Retail"]},
+                {name:"Urban",sub:[]},
+                {name:"Interior", sub:["Residential Private"]}
+              ]
 
   });
 
+  Template.portfolio.events({
+    "click .parentCat": function(event){
+      Session.set("currCat",event.target.innerHTML);
+      Session.set("currSubCat",null);
+    },
+    "click .childCat": function(event){
+      Session.set("currSubCat",event.target.innerHTML);
+    }
+  });
 }
 
 
