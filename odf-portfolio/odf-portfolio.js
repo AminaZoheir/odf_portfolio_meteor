3b92a3cf116e954d7035b8503ab9a4e075516cea
Amina <amina.zoheir@gmail.com>
2015-12-06 19:01:50 +0200

    add admin

--- odf-portfolio/odf-portfolio.js
+++ odf-portfolio/odf-portfolio.js
@@ -1,11 +1,14 @@
 Projects = new Mongo.Collection("projects");
-// projects.
+
+Images = new FS.Collection("images", {
+  stores: [new FS.Store.FileSystem("images", {path: "~/uploads"})]
+});
 
 Router.route('/',{
   template: 'home'
 });
 Router.configure({
-  layoutTemplate: 'main-layout'
+  layoutTemplate: 'main'
 });
 Router.route('/portfolio/:_id', {
     template: 'projectPage',
@@ -16,6 +19,10 @@ Router.route('/portfolio/:_id', {
     }
 });
 Router.route('/portfolio');
+Router.route('/contact');
+Router.route('/about');
+Router.route('/news');
+Router.route('/admin');
 
 if (Meteor.isClient) {
 
@@ -30,12 +37,20 @@ if (Meteor.isClient) {
       }
       return Projects.find({});
     },
-    categories: [
+    // categories: [
+    //             {name:"Architecture",sub:["Complexes","Hotels","Residential Buildings","Residential Compounds","Admin","Retail"]},
+    //             {name:"Urban",sub:[]},
+    //             {name:"Interior", sub:["Residential Private"]}
+    //           ]
+
+  });
+
+  Template.registerHelper('categories', function(){
+    return [
                 {name:"Architecture",sub:["Complexes","Hotels","Residential Buildings","Residential Compounds","Admin","Retail"]},
                 {name:"Urban",sub:[]},
                 {name:"Interior", sub:["Residential Private"]}
-              ]
-
+              ];
   });
 
   Template.portfolio.events({
@@ -47,11 +62,63 @@ if (Meteor.isClient) {
       Session.set("currSubCat",event.target.innerHTML);
     }
   });
+
+  Template.admin.helpers({
+    photos: function(){
+      
+      return Session.get("files");
+    }
+  });
+  Template.admin.events({
+    'change .fileInput': function(event, template){
+      Session.set("files", event.target.files);
+    },
+    'submit .adminform':function(event){
+      event.preventDefault();
+
+      var title = event.target.title.value;
+      var desc = event.target.desc.value;
+      var cat = event.target.optcat.value;
+      var subcat = event.target.optsubcat.value;
+
+      console.log(title);
+      console.log(desc);
+      // console.log(optcat);
+      // console.log(optsubcat);
+      console.log(Session.get("files"));
+
+      // Insert a task into the collection
+
+      // var task = Tasks.insert({
+
+      //   text: text,
+
+      //   createdAt: new Date() // current time
+
+      // });
+
+      // for (var i = 0, ln = files.length; i < ln; i++) {
+      //   var fileObj = Uploads.insert(files[i], function (err, fileObj) {
+      //     // Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
+      //   });
+      //   Uploads.update(fileObj._id,{
+      //     $set: {task: task}
+      //   });
+      // }
+      event.target.title.value = "";
+    }
+  });
 }
 
 
 
 if (Meteor.isServer) {
+  Images.allow({
+    'insert': function () {
+      // add custom authentication code here
+      return true;
+    }
+  });
   Meteor.startup(function () {
     // code to run on server at startup
   });
