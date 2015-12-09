e184b5bb6d93f709bb9dcf0e3cc5206f641ccc67
Mohamed Salah <msalah93@Mohameds-MacBook-Pro-5.local>
2015-12-09 23:38:31 +0200

    Added google maps

--- odf-portfolio/odf-portfolio.js
+++ odf-portfolio/odf-portfolio.js
@@ -106,6 +106,27 @@ if (Meteor.isClient) {
     }
   });
 
+  Template.contact.rendered = function(){
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 13,
+      center: {lat: -34.397, lng: 150.644}
+    });
+    var geocoder = new google.maps.Geocoder();
+    var address = '8 Dr. Hegazy Street, Agouza, Giza, Egypt';
+    geocoder.geocode({'address': address}, function(results, status) {
+      if (status === google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location,
+          width: 300 // zy fl .css file
+        });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  };
+
   Template.admin.helpers({
     photos: function(){
       return Images.find({project: Session.get("currProj")});
