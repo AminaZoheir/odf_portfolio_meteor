9babf9f94426fe1aa53757a1ddaa7b10317af30f
Amina <amina.zoheir@gmail.com>
2015-12-28 19:47:18 +0200

    Add modal dialogs for edit, add admin tab in navbar if admin

--- odf-portfolio/news.js
+++ odf-portfolio/news.js
@@ -2,6 +2,96 @@ if(Meteor.isClient){
   Template.news.helpers({
     news: function(){
       return News.find({}, {sort: {createdAt: -1}});
+    },
+    edit: function(){
+      return Session.get('edit-news');
+    },
+    currnews: function(){
+      return (this._id == Session.get('currnews'));
+    },
+    subCats: function(){
+      if(Session.get('cat-news')){
+       return getSubCategoryList(Session.get('cat-news'));
+      }
+      return getSubCategoryList('Interior');
+    },
+    sameCat: function(name){
+      return(name == Session.get('cat-news'));
+    },
+    sameSub: function(name){
+      return(name == Session.get('subcat-news'));
+    },
+    sameCountry: function(name){
+      return(name == Session.get('country-news'));
+    },
+    projects: function(){
+      if(Session.get("cat-news")){
+        if(Session.get("subcat-news"))
+          return Projects.find({category:Session.get("cat-news"), subcategory:Session.get("subcat-news")});
+        return Projects.find({category:Session.get("cat-news")});
+      }
+      return Projects.find({category:'Interior', subcategory: getSubCategoryList('Interior')[0]});
+    },
+    sameProj: function(name){
+      return(name == Session.get('project-news'));
     }
   });
+
+	Template.news.events({
+	'change .cat-admin': function(event){
+      Session.set('cat-news', event.target.value);
+      Session.set('subcat-news', getSubCategoryList(event.target.value)[0]);
+    },
+      'submit .adminform':function(event, template){
+        event.preventDefault();
+        var title = event.target.title.value;
+        var desc = event.target.desc.value;
+        var status = event.target.status.value;
+        var cat = template.find('input:radio[name=optcat]:checked').value;
+        var subcat = template.find('[name=optsubcat]').value;
+        var ishome = template.find('[name=ishome]').checked;
+        var country = template.find('[name=optcountry]').value;
+        var files =  event.target.photoupload.files;
+        var index = Projects.find({}).count();
+        var proj = template.find('[name=optproj]').options[template.find('[name=optproj]').selectedIndex].getAttribute('projId');
+
+        var news = News.update(Session.get('currproj'), {
+          $set: {title: title,
+          desc: desc,
+          category: cat,
+          subcategory: subcat,
+          ishome: ishome,
+          country: country,
+          projId: proj,
+          status: status}
+        });
+      for (var i = 0, ln = files.length; i < ln; i++) {
+
+          var fileObj = Images.insert(files[i], function (err, fileObj) {
+          });
+          Images.update(fileObj._id,{
+            $set: {project: Session.get('currnews')}
+          });
+      }
+      Session.set('edit-news', false);
+      Session.set('currnews', null);
+      Session.set('cat-news', null);
+      Session.set('subcat-news', null);
+      Session.set('country-news', null);
+      },
+      'click .close-edit-news': function(event){
+      	Session.set('edit-news', false);
+	    Session.set('currnews', null);
+	    Session.set('cat-news', null);
+	    Session.set('subcat-news', null);
+	    Session.set('country-news', null);
+      }
+	});
+
+	function getSubCategoryList(category){
+	    for(var i = 0;i<categories.length;i++){
+	      if(categories[i].name == category)
+	        return categories[i].sub;
+	    }
+	}
 }
--- odf-portfolio/portfolio.js
+++ odf-portfolio/portfolio.js
@@ -8,6 +8,27 @@ if (Meteor.isClient) {
         return Projects.find({category:Session.get("currCat")},{sort: {index: 1}});
       }
       return Projects.find({},{sort: {index: 1}});
+    },
+    edit: function(){
+      return Session.get('edit');
+    },
+    currproj: function(){
+      return (this._id == Session.get('currproj'));
+    },
+    subCats: function(){
+      if(Session.get('cat')){
+       return getSubCategoryList(Session.get('cat'));
+      }
+      return getSubCategoryList('Interior');
+    },
+    sameCat: function(name){
+      return(name == Session.get('cat'));
+    },
+    sameSub: function(name){
+      return(name == Session.get('subcat'));
+    },
+    sameCountry: function(name){
+      return(name == Session.get('country'));
     }
   });
 
@@ -18,7 +39,60 @@ if (Meteor.isClient) {
     },
     "click .childCat": function(event){
       Session.set("currSubCat",event.target.innerHTML);
-    }
-  });
+    },
+    'change .cat-admin': function(event){
+      console.log("amina");
+      Session.set('cat', event.target.value);
+    },
+      'submit .adminform':function(event, template){
+        event.preventDefault();
+        var title = event.target.title.value;
+        var desc = event.target.desc.value;
+        var cat = template.find('input:radio[name=optcat]:checked').value;
+        var subcat = template.find('[name=optsubcat]').value;
+        var ishome = template.find('[name=ishome]').checked;
+        var country = template.find('[name=optcountry]').value;
+        var files =  event.target.photoupload.files;
+        var index = Projects.find({}).count();
 
+        var project = Projects.update(Session.get('currproj'), {
+          $set: {title: title,
+          desc: desc,
+          category: cat,
+          subcategory: subcat,
+          ishome: ishome,
+          country: country}
+        });
+      for (var i = 0, ln = files.length; i < ln; i++) {
+
+          var fileObj = Images.insert(files[i], function (err, fileObj) {
+          });
+          Images.update(fileObj._id,{
+            $set: {project: Session.get('currproj')}
+          });
+      }
+      Session.set('edit', false);
+      Session.set('currproj', null);
+      Session.set('cat', null);
+      Session.set('subcat', null);
+      Session.set('country', null);
+      // var el =  document.getElementById('#editprojModal');
+      // el.parentNode.removeChild(el);
+        // window.scrollTo(0, 0);
+        // tempAlert("Project Added Successfully",2000);
+      },
+      'click .close-edit':function(event){
+        Session.set('edit', false);
+        Session.set('currproj', null);
+        Session.set('cat', null);
+        Session.set('subcat', null);
+        Session.set('country', null);
+      }
+  });
+  function getSubCategoryList(category){
+    for(var i = 0;i<categories.length;i++){
+      if(categories[i].name == category)
+        return categories[i].sub;
+    }
+  }
 }
