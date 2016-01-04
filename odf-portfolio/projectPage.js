24cf40bb8e33c1921f4f7dea22291ce0258df7b5
Amina <amina.zoheir@gmail.com>
2016-01-05 01:13:43 +0200

    Edit forms in admin, adjust login form, add video conrols in projectpage

--- odf-portfolio/projectPage.js
+++ odf-portfolio/projectPage.js
@@ -17,4 +17,42 @@ if(Meteor.isClient){
       return index == 0;
     }
   });
+
+  Template.projectPage.events({
+    'click .proj-video': function(event){
+      var video = event.target;
+      if (video.paused == true) {
+        video.play();
+        var play = $(video).siblings('.proj-play');
+        // play.attr('visible', true);
+        $(play).fadeIn("slow", function(){
+          $(play).fadeOut("slow");
+        });
+       //  setTimeout(function(){
+       //    var play = $(video).siblings('.proj-play');
+       //  play.attr('visible', false);
+       // },1000);
+        // var pause = $(video).siblings('.proj-pause');
+        // pause.attr('visible', false);
+      }else{
+        video.pause();
+        var play = $(video).siblings('.proj-play');
+        var pause = $(video).siblings('.proj-pause');
+        // play.attr('visible', true);
+        $(pause).fadeIn("slow", function(){
+          $(pause).fadeOut("slow");
+          $(play).fadeIn("slow");
+        });
+        // var pause = $(video).siblings('.proj-pause');
+        // pause.attr('visible', true);
+        // var play = $(video).siblings('.proj-play');
+        // play.attr('visible', false);
+      }
+    },
+    'ended .proj-video':function(event){
+      var video = event.target;
+        var play = $(video).siblings('.proj-play');
+        $(play).fadeIn("slow");
+    }
+  });
 }
