9ef72dd266759fede93aa06a1ff5be58094798aa
Amina <amina.zoheir@gmail.com>
2016-01-25 13:22:36 +0200

    Add removed .carousel in home.rendered

--- odf-portfolio/home.js
+++ odf-portfolio/home.js
@@ -7,8 +7,7 @@ if(Meteor.isClient){
       return index == 0;
     }
   });
-	// Template.home.rendered = function () {
-	// 	this.$('#home-carousel').carousel();
- //    document.title = "ODF";
-	// };
+	Template.home.rendered = function () {
+		this.$('#home-carousel').carousel();
+	};
 }
